555af343f2eff0af1b5ff7057de1726f07bdbd94
Nikhil-Jalandhra <jneek36@gmail.com>
2024-04-30 12:47:37 +0530

    update todo functionality

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -5,6 +5,7 @@ import { TbEdit } from "react-icons/tb";
 import { MdDeleteSweep } from "react-icons/md";
 import { LuMessageSquarePlus } from "react-icons/lu";
 import { IoIosSave } from "react-icons/io";
+import { FaFeatherAlt } from "react-icons/fa";
 
 function Todos() {
 
@@ -15,6 +16,8 @@ function Todos() {
   const [save, setsave] = useState(false);
   const [tempId, setTempId] = useState("");
   const editRef = useRef(null);
+
+
 
     const newTodo = (e) =>{
       if (text !== "") { 
@@ -34,6 +37,7 @@ function Todos() {
         setText(clickedTodo.Text)
         setTempId(clickedTodo.Id)
         setsave(true)
+        setTextCounter(clickedTodo.Text.length)
       }   
     }
 
@@ -53,13 +57,22 @@ function Todos() {
         }
       }
 
+      const checkedTodo = () => {
+        const checked = document.querySelectorAll('input[type="radio"]:checked') 
+        console.log("Helo")
+        console.log(checked.parentElement)
+
+      }
+
   return (
     <div className="w-full pb-[200px] min-h-screen bg-zinc-400 flex flex-col items-center">
-      <div className="w-[600px] bg-lime-300 px-[30px] py-[20px] mt-[100px] rounded-xl">
-        <h1 className='text-[50px] font-bold text-center mb-[10px]'>Manage Your Todos</h1>
-        <div>
+      <div className=" lg:w-[600px] w-[350px] px-2 py-[20px] mt-[50px] rounded-xl">
+        <div className=' w-full justify-center items-center flex lg:text-[50px] text-[30px] mb-[10px]'>
+        <h1 className='font-bold'>Manage Your Todo</h1>
+        <span className='lg:ml-[15px] ml-[10px] text-[#0D009F] '><FaFeatherAlt /></span> 
+        </div>
           <form onSubmit={handelSubmit} >
-            <div className=' select-none py-[10px] flex border-[2px] border-black rounded-full w'>
+            <div className=' bg-white  py-[10px] flex border-[2px] border-black rounded-full '>
               <input
               ref={editRef}
               maxLength={"200"}
@@ -82,26 +95,29 @@ function Todos() {
           <div>
             {todos.map((todo)=>(
               <label key={todo.Id} htmlFor={todo.Text}>
-                <div className='select-none w-full flex items-start justify-start rounded-2xl bg-amber-400 mt-[20px] py-[10px] px-[15px] text-[17px]'>
-                  <input type="checkbox" maxLength={"50em"} id={todo.Text} className='mr-[10px] mt-[8px] w-[30px] h-[30px]' />
-                    <div className='w-full text-justify rounded-lg px-[15px] py-[10px] bg-white mr-[10px]'>
+                <div className='w-full h-[1px] bg-gray-500 my-3'></div>
+                <div className='select-none w-full flex items-start justify-start rounded-2xl bg-rose-500 lg:mt-[10px] lg:py-[10px] py-[5px] lg:px-[15px] px-[10px] text-[17px]'>
+                  <input type="checkbox" onChange={checkedTodo} id={todo.Text} className='mr-[10px] mt-[8px] w-[30px] h-[30px]' />
+                    <div className='w-full text-justify rounded-lg lg:px-[15px] px-[10px] py-[10px] bg-white mr-[10px]'>
                       <h1 id={todo.Id} >{todo.Text}</h1>
                     </div>
-                  <button
-                  onClick={()=> getTodo(todo)}
-                  disabled={save}
-                  className=' text-[30px] pr-[5px] mt-[8px] hover:text-blue-600'
-                  ><TbEdit /></button>
-                  <button
-                  disabled={save}
-                  onClick={()=>(dispatch(deleteTodo(todo.Id)))} 
-                  className=' text-[30px] pr-[5px] mt-[8px] hover:text-red-600'
-                  ><MdDeleteSweep /></button>
+                  <div className='lg:flex md:flex'>
+                    <button
+                    onClick={()=> getTodo(todo)}
+                    disabled={save}
+                    className=' text-[30px] pr-[5px] mt-[8px] hover:text-blue-600'
+                    ><TbEdit /></button>
+                    <button
+                    disabled={save}
+                    onClick={()=>(dispatch(deleteTodo(todo.Id)))} 
+                    className=' text-[30px] pr-[5px] mt-[8px] hover:text-red-600'
+                    ><MdDeleteSweep /></button>
+                  </div>
                 </div>
               </label>
             ))}
           </div>
-        </div>
+        
       </div>
     </div>
   );
